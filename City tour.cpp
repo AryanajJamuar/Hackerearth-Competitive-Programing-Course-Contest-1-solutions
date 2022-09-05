876444ba9d7aee253f5bfeb78b567ab9037d8545
AryanajJamuar <86473851+AryanajJamuar@users.noreply.github.com>
2022-09-05 11:49:12 +0530

    Create City tour.cpp

---
+++ City tour.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath> 
+#include <algorithm>
+#include <vector>
+using namespace std ;
+typedef long long int ll ;
+int main()
+{ll t  ;
+scanf("%lld",&t) ;
+while(t--)
+{ ll n ;
+scanf("%lld",&n) ;
+printf("%lld\n",n*4) ;
+}
+  
+    return 0;
+}
